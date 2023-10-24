2c86411c7846fe30d8ce49d32a53a1b2c602c397
tessakapeller <tmkapell@uark.edu>
2023-10-24 14:20:43 -0500

    player action screen

--- src/main/java/com/example/teammain/HelloApplication.java
+++ src/main/java/com/example/teammain/HelloApplication.java
@@ -1,21 +1,18 @@
 package com.example.teammain;
 
-import com.example.teammain.DBUtils.DBMethods;
 import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-import com.example.teammain.MainController;
 
 import java.io.IOException;
 import javafx.util.Duration;
 
 public class HelloApplication extends Application {
 
-    static MainController myControllerHandle;
+    static PlayerEntryController myControllerHandle;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -28,22 +25,35 @@ public class HelloApplication extends Application {
         PauseTransition pause = new PauseTransition(Duration.seconds(3));// set this to 3 when done testing
         pause.setOnFinished(event -> {
             // After 3 seconds, switch to PlayerEntry.fxml
-            try {
-                FXMLLoader playerEntryLoader = new FXMLLoader(HelloApplication.class.getResource("/PlayerEntry.fxml"));
-                Scene playerEntryScene = new Scene(playerEntryLoader.load());
-                myControllerHandle = (MainController)playerEntryLoader.getController();
+            try
+            {
+//                FXMLLoader playerEntryLoader = new FXMLLoader(HelloApplication.class.getResource("/PlayerEntry.fxml"));
+//                Scene playerEntryScene = new Scene(playerEntryLoader.load());
+//                myControllerHandle = (PlayerEntryController)playerEntryLoader.getController();
+//
+//                playerEntryScene.setOnKeyPressed((KeyEvent ke) -> { // Create a key event that execute when any key pressed from your keyboard
+//                    myControllerHandle.keyEventHandler(ke);
+//                });
+//                stage.setTitle("Hello!");
+//                stage.setScene(playerEntryScene);
+                FXMLLoader gameActionLoader = new FXMLLoader(HelloApplication.class.getResource("/PlayerActionDisplay.fxml"));
+                Scene gameActionScreen = new Scene(gameActionLoader.load());
+                stage.setScene(gameActionScreen);
+                stage.setTitle("Game Action");
 
-                playerEntryScene.setOnKeyPressed((KeyEvent ke) -> { // Create a key event that execute when any key pressed from your keyboard
-                    myControllerHandle.keyEventHandler(ke);
-                });
-                stage.setTitle("Hello!");
-                stage.setScene(playerEntryScene);
-
-            } catch (IOException e) {
+            } catch (IOException e)
+            {
                 e.printStackTrace();
             }
         });
+
         pause.play();
+
+        //when button pressed
+//        FXMLLoader gameActionLoader = new FXMLLoader(HelloApplication.class.getResource("/PlayerActionDisplay.fxml"));
+//        Scene gameActionScreen = new Scene(gameActionLoader.load());
+//        stage.setScene(gameActionScreen);
+//        stage.setTitle("Game Action");
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/teammain/PlayerActionTable.java
@@ -0,0 +1,26 @@
+package com.example.teammain;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+
+public class PlayerActionTable {
+
+    private ObservableList<Player> players = FXCollections.observableArrayList();
+
+    @FXML
+    public void actionTables(TableView<Player> tableView) {
+
+        TableColumn<Player, String> codeNameColumn = new TableColumn<>("CodeName");
+        codeNameColumn.setCellValueFactory(cellData -> cellData.getValue().codeNameProperty()); // Updated line
+        TableColumn<Player, String> playerScore = new TableColumn<>("PlayerScore");
+        codeNameColumn.setCellValueFactory(cellData -> cellData.getValue().scoreProperty());  //cellData.getValue().codeNameProperty()); // Updated line
+        codeNameColumn.prefWidthProperty().bind(tableView.widthProperty().multiply(0.7));
+        playerScore.prefWidthProperty().bind(tableView.widthProperty().multiply(0.3));
+        tableView.getColumns().setAll(codeNameColumn, playerScore);
+        tableView.setItems(players);
+
+
+    }
+}
